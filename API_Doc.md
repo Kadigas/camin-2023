0c4584224e3303ee08cb18ea5765154fb50a40cb
Nopalev <faadhilah56@gmail.com>
2023-03-23 23:53:51 +0700

    Finishing API and Documentation

---
+++ API_Doc.md
@@ -0,0 +1,343 @@
+# API Documentation
+
+## ping (GET)
+
+Endpoint:
+
+```txt
+/api/ping
+```
+
+### Request
+
+Standard header, no body.
+
+### Response
+
+#### 200 OK
+
+Indicator that API endpoint created successfully.
+
+```json
+"Pong"
+```
+
+## camin (GET)
+
+Endpoint:
+
+```txt
+/api/camin
+```
+
+### Request
+
+Standard header, no body.
+
+### Response
+
+#### 200 OK
+
+Successfully get all camin data, stored in JSON Array `camins`.
+
+```json
+{
+    "status": true,
+    "camins": [
+        {
+            "nama": string,
+            "angkatan_id": int,
+            "nrp": string,
+            "jurusan": string
+        }
+    ]
+}
+```
+
+## camin/{camins_id} (GET)
+
+Endpoint:
+
+```txt
+/api/camin/{camins_id}
+```
+
+### Request
+
+Standard header, no body.
+
+### Response
+
+#### 200 OK
+
+Successfully get camin data with inserted id.
+
+```json
+{
+    "status": true,
+    "camin": {
+        "nama": string,
+        "angkatan_id": int,
+        "nrp": string,
+        "jurusan": string
+    }
+}
+```
+
+#### 404 Not Found
+
+Camin data with inserted id doesn't exist.
+
+```json
+{
+    "status": "error",
+    "error": "Data camin not found"
+}
+```
+
+## camin (POST)
+
+Endpoint:
+
+```txt
+/api/camin
+```
+
+### Request
+
+Standard header with body.
+
+```json
+{
+    "nama": string,
+    "angkatan_id": int,
+    "nrp": string,
+    "jurusan": string
+}
+```
+
+### Response
+
+#### 201 Created
+
+Successfully created camin with given data.
+
+```json
+{
+    "status": true,
+    "message": "Camin created"
+}
+```
+
+#### 400 Bad request
+
+Failed validation in at least one of following fields mentioned below.
+
+```json
+{
+    "status": "error",
+    "error": {
+        "nama": [
+            "Name can't be empty!"
+        ],
+        "nrp": [
+            "NRP can't be empty!"
+        ],
+        "jurusan": [
+            "Jurusan can't be empty!"
+        ],
+        "angkatan_id": [
+            "Please choose your angkatan"
+        ]
+    }
+}
+```
+
+#### 409 Conflict
+
+Conflict in NRP (the NRP inserted already exist).
+
+```json
+{
+    "status": "error",
+    "error": "Conflicting NRP"
+}
+```
+
+## camins (POST)
+
+Endpoint:
+
+```txt
+/api/camins
+```
+
+### Request
+
+Standard header with body, able to receive multiple data.
+
+```json
+[
+    {
+        "nama": string,
+        "angkatan_id": int,
+        "nrp": string,
+        "jurusan": string
+    },
+    {
+        "nama": string,
+        "angkatan_id": int,
+        "nrp": string,
+        "jurusan": string
+    }
+]
+```
+
+### Response
+
+#### 200 OK
+
+The API will always respond with `200 OK` with response body contains array of responses respective to the requests:
+
+- 201, Successfully created camin with given data.
+- 400, Failed validation in all mentioned error(s) in the respective request.
+- 409, Conflict in NRP (the NRP inserted already exist).
+
+example below is a response to a request with 4 data (1 sucessfull, 2 failed validation (with several variations), and 1 conflicting NRP).
+
+```json
+[
+    [
+        201,
+        "Camin created"
+    ],
+    [
+        400,
+        "Validation failed with error(s): Name can't be empty, NRP can't be empty, Jurusan can't be empty, Please choose your angkatan."
+    ],
+    [
+        400,
+        "Validation failed with error(s): NRP can't be empty, Jurusan can't be empty, Please choose your angkatan."
+    ],
+    [
+        409,
+        "Camin not created (Conflicting NRP)"
+    ]
+]
+```
+
+## camin/{camins_id} (PUT)
+
+Endpoint:
+
+```txt
+/api/camin/{camins_id}
+```
+
+### Request
+
+Standard header with body.
+
+```json
+{
+    "nama": string,
+    "angkatan_id": int,
+    "nrp": string,
+    "jurusan": string
+}
+```
+
+### Response
+
+#### 201 Created
+
+Successfully update camin data with inserted id.
+
+```json
+{
+    "status": true,
+    "message": "Updated"
+}
+```
+
+#### 400 Bad Request
+
+Failed validation in at least one of following fields mentioned below.
+
+```json
+{
+    "status": "error",
+    "error": {
+        "nama": [
+            "Name can't be empty!"
+        ],
+        "nrp": [
+            "NRP can't be empty!"
+        ],
+        "jurusan": [
+            "Jurusan can't be empty!"
+        ],
+        "angkatan_id": [
+            "Please choose your angkatan"
+        ]
+    }
+}
+```
+
+#### 404 Not Found
+
+Camin data with inserted id doesn't exist.
+
+```json
+{
+    "status": "error",
+    "error": "Data camin not found"
+}
+```
+
+#### 409 Conflict
+
+Conflict in NRP (the NRP inserted already exist).
+
+```json
+{
+    "status": "error",
+    "error": "Conflicting NRP"
+}
+```
+
+## camin/{camins_id} (DELETE)
+
+Endpoint:
+
+```txt
+/api/camin/{camins_id}
+```
+
+### Request
+
+Standard header, no body.
+
+### Response
+
+#### 200 OK
+
+Successfully deleted camin data with inserted id.
+
+```json
+{
+    "status": true,
+    "message": "deleted"
+}
+```
+
+#### 404 Not Found
+
+Camin data with inserted id doesn't exist.
+
+```json
+{
+    "status": "error",
+    "error": "Data camin not found"
+}
+```
--- app/Http/Controllers/Api/CaminAPIController.php
+++ app/Http/Controllers/Api/CaminAPIController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\CaminResource;
 use App\Models\Camin;
+use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
@@ -22,7 +23,7 @@ class CaminAPIController extends Controller
         return response()->json([
             'status' => true,
             'camins' => $camins
-        ]);
+        ], 200);
     }
 
     /**
@@ -47,17 +48,32 @@ class CaminAPIController extends Controller
         ]);
 
         if ($validator->fails()) {
-            return response()->json($validator->errors(), 400);
+            return response()->json([
+                'status' => 'error',
+                'error' => $validator->errors()
+            ], 400);
         }
 
-        Camin::create([
-            'nama' => $request->nama,
-            'angkatan_id' => $request->angkatan_id,
-            'nrp' => $request->nrp,
-            'jurusan' => $request->jurusan,
-        ]);
+        try{
+            Camin::create([
+                'nama' => $request->nama,
+                'angkatan_id' => $request->angkatan_id,
+                'nrp' => $request->nrp,
+                'jurusan' => $request->jurusan,
+            ]);
+            
+            return response()->json([
+                'status' => true,
+                'message' => 'Camin created'
+            ], 201);
+        }
+        catch(Exception $e){
+            return response()->json([
+                'status' => 'error',
+                'error' => 'Conflicting NRP'
+            ], 409);
+        }
 
-        return response()->json('', 200);
     }
 
     public function store_multiple(Request $request){
@@ -72,25 +88,32 @@ class CaminAPIController extends Controller
                 'angkatan_id' => 'required',
             ],
             [
-                'nama.required' => 'Name can\'t be empty!',
-                'nrp.required' => 'NRP can\'t be empty!',
-                'jurusan.required' => 'Jurusan can\'t be empty!',
+                'nama.required' => 'Name can\'t be empty',
+                'nrp.required' => 'NRP can\'t be empty',
+                'jurusan.required' => 'Jurusan can\'t be empty',
                 'angkatan_id' => 'Please choose your angkatan',
             ]);
             
             if ($validator->fails()) {
-                array_push($response, 'Validation failed');
+                $errors = $validator->errors()->all();
+                $str = 'Validation failed with error(s): ' . implode(", ", $errors);
+                array_push($response, [400, $str . '.']);
             }
             else{
-                array_push($response, 'Validation successfull');
+                try{
+                    Camin::create([
+                        'nama' => $camin['nama'],
+                        'angkatan_id' => $camin['angkatan_id'],
+                        'nrp' => $camin['nrp'],
+                        'jurusan' => $camin['jurusan'],
+                    ]);
+                    array_push($response, [201, 'Camin created']);
+                }
+                catch(Exception $e){
+                    array_push($response, [409, 'Camin not created (Conflicting NRP)']);
+                }
             }
             
-            Camin::create([
-                'nama' => $camin['nama'],
-                'angkatan_id' => $camin['angkatan_id'],
-                'nrp' => $camin['nrp'],
-                'jurusan' => $camin['jurusan'],
-            ]);
         }
 
         return response()->json($response, 200);
@@ -104,11 +127,18 @@ class CaminAPIController extends Controller
      */
     public function show($id)
     {
-        $camin = new CaminResource(Camin::findorfail($id));
-        return response()->json([
-            'status' => true,
-            'camin' => $camin
-        ]);
+        try {
+            $camin = new CaminResource(Camin::findorfail($id));
+            return response()->json([
+                'status' => true,
+                'camin' => $camin
+            ], 200);
+        } catch (Exception $e) {
+            return response()->json([
+                'status' => 'error',
+                'error' => 'Data camin not found'
+            ], 404);
+        }
     }
 
     /**
@@ -134,21 +164,41 @@ class CaminAPIController extends Controller
         ]);
 
         if ($validator->fails()) {
-            return response()->json($validator->errors(), 400);
+            return response()->json([
+                'status' => 'error',
+                'error' => $validator->errors()
+            ], 400);
         }
 
-        $camin = Camin::findorfail($id);
-        
-        $camin_data = [
-            'nama' => $request->nama,
-            'angkatan_id' => $request->angkatan_id,
-            'nrp' => $request->nrp,
-            'jurusan' => $request->jurusan,
-        ];
-
-        $camin->update($camin_data);
-
-        return response()->json('', 200);
+        try{
+            $camin = Camin::findorfail($id);
+            
+            $camin_data = [
+                'nama' => $request->nama,
+                'angkatan_id' => $request->angkatan_id,
+                'nrp' => $request->nrp,
+                'jurusan' => $request->jurusan,
+            ];
+    
+            $camin->update($camin_data);
+    
+            return response()->json([
+                'status' => true,
+                'message' => 'Updated'
+            ], 201);
+        }
+        catch(Exception $e){
+            if($e->getCode() == 0){
+                return response()->json([
+                    'status' => 'error',
+                    'error' => 'Data camin not found'
+                ], 404);
+            }
+            return response()->json([
+                'status' => 'error',
+                'error' => 'Conflicting NRP'
+            ], 409);
+        }
     }
 
     /**
@@ -159,7 +209,19 @@ class CaminAPIController extends Controller
      */
     public function destroy($id)
     {
-        $camin = Camin::findorfail($id);
-        $camin->delete();
+        try{
+            $camin = Camin::findorfail($id);
+            $camin->delete();
+            return response()->json([
+                'status' => true,
+                'message' => 'deleted'
+        ], 200);
+        }
+        catch(Exception $e){
+            return response()->json([
+                'status' => 'error',
+                'error' => 'Data camin not found'
+            ], 404);
+        }
     }
 }
